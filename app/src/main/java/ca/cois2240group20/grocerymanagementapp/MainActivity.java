6e349a9e6d0b05f71e09b11c76bffb3295221254
Taylor McLean <taylormclean15@gmail.com>
2019-03-29 19:14:10 -0400

    Converted from ActionBar (deprecated) to Toolbar to resolve compatibility issues

--- app/src/main/java/ca/cois2240group20/grocerymanagementapp/MainActivity.java
+++ app/src/main/java/ca/cois2240group20/grocerymanagementapp/MainActivity.java
@@ -1,11 +1,13 @@
 package ca.cois2240group20.grocerymanagementapp;
 
-import android.app.ActionBar;
-import android.support.v4.app.FragmentActivity;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
     // The viewPager is the main layout widget that will show all the different page fragments
     ViewPager viewPager;
     // The pagerAdapter is what handles which pages get shown in the viewPager
@@ -20,48 +22,56 @@ public class MainActivity extends FragmentActivity {
         viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setAdapter(pagerAdapter);
 
-        // Generate action bar for app
-        final ActionBar actionbar = getActionBar();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            toolbar.setTitle(R.string.app_name);
+            setSupportActionBar(toolbar);
+        }
 
-        // To specify that the action bar will use tabular navigation
-        actionbar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        ActionBar actionBar = getSupportActionBar();
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
         // A tab listener is needed to specify which actions to take when the user selects a tab
-        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+        TabLayout.OnTabSelectedListener listener = new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onTabSelected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
+            public void onTabSelected(TabLayout.Tab tab) {
                 // Uses the pagerAdapter getItem() to determine what fragment is at the position of the selected tab
                 viewPager.setCurrentItem(tab.getPosition());
             }
 
             @Override
-            public void onTabUnselected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
+            public void onTabUnselected(TabLayout.Tab tab) {
                 // Nothing really needs to be done here. We could potentially hide the page,
                 // but that wouldn't be particularly useful to the user. This method needs to exist
                 // either way to satisfy the TabListener abstract class methods.
             }
 
             @Override
-            public void onTabReselected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
+            public void onTabReselected(TabLayout.Tab tab) {
                 // Similarly, nothing really needs to be done.
             }
         };
 
+        tabLayout.addOnTabSelectedListener(listener);
+
         // This next few lines of code sets up a listener for our viewPager that changes tabs
         // when the user swipes through pages on their phone.
-        viewPager.setOnPageChangeListener(
+        viewPager.addOnPageChangeListener(
                 new ViewPager.SimpleOnPageChangeListener() {
                     @Override
                     public void onPageSelected(int position) {
                         // When swiping between pages, select the
                         // corresponding tab.
-                        getActionBar().setSelectedNavigationItem(position);
+                        viewPager.setCurrentItem(position);
+
                     }
                 });
 
         // We need to add the 3 action bar tabs
-        actionbar.addTab(actionbar.newTab().setText("Home").setTabListener(tabListener));
-        actionbar.addTab(actionbar.newTab().setText("Inventory").setTabListener(tabListener));
-        actionbar.addTab(actionbar.newTab().setText("Grocery List").setTabListener(tabListener));
+        tabLayout.addTab(tabLayout.newTab().setText("Home"));
+        tabLayout.addTab(tabLayout.newTab().setText("Inventory"));
+        tabLayout.addTab(tabLayout.newTab().setText("Grocery List"));
     }
 }
